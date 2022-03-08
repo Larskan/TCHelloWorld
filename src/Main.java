2d0adbdb19d019c5d6f27a678730f67ff90e1f17
Larskan <101105018+Larskan@users.noreply.github.com>
2022-03-08 12:11:57 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane p = new Pane();
+        Scene scene = new Scene(p,300,400);
+
+        Button btn = new Button("Click me");
+        btn.setOnAction(event -> System.out.println("Hello World"));
+
+        p.getChildren().add(btn);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+}
